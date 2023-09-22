e3f8d1526a58fd8212ca14a8d52a473307154313
Md Shahnewaj Sheikh <shahanewajshaikh786@gmail.com>
2023-09-23 02:23:29 +0600

    variable type declear null and undefine

--- Node JS project/TestProject1/index.js
+++ Node JS project/TestProject1/index.js
@@ -4,7 +4,7 @@ console.log("Learn Automation Testing"); // read for node
 //Variable in node
 var x = "Test Automation is Fun" // Good practice is not to use var for declear variable
 let y = "Mobile Automation is Fun"
-const z = "Load testing is Fun"
+const z = "Load testing is Fun" // for good practice
 
 
 
@@ -12,3 +12,13 @@ const z = "Load testing is Fun"
 console.log(x);
 console.log(y);
 console.log(z);
+console.log(typeof(z)); //for check any variable type. 
+
+
+let a = 3;
+a = undefined; // for remove variable
+console.log(a);
+
+let b = 33;
+b = null; // for remove variable
+console.log(b);
