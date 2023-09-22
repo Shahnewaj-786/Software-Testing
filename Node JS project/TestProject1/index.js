126503f134cbc436c801d42991dc1ef696685bdc
Md Shahnewaj Sheikh <shahanewajshaikh786@gmail.com>
2023-09-23 02:05:54 +0600

    variable in js

--- Node JS project/TestProject1/index.js
+++ Node JS project/TestProject1/index.js
@@ -2,4 +2,13 @@ console.log("Learn Automation Testing"); // read for node
 
 
 //Variable in node
-var x = "Test Automation is Fun"
+var x = "Test Automation is Fun" // Good practice is not to use var for declear variable
+let y = "Mobile Automation is Fun"
+const z = "Load testing is Fun"
+
+
+
+
+console.log(x);
+console.log(y);
+console.log(z);
